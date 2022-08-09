e69c9bf109acb2ae9bd8748ba97baa2cedd612f4
Parker Thomson <pdthomson1239@gmail.com>
2022-08-08 20:16:00 -0400

    add code in models as well as view to complete user story 6

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -6,8 +6,28 @@ class InvoiceItem < ApplicationRecord
 
     has_one :merchant, through: :item
     has_many :transactions, through: :invoice
-
+    has_many :bulk_discounts, through: :merchant
     validates_presence_of :quantity
     validates_presence_of :unit_price
     validates_presence_of :status
+
+    def best_discount
+      bulk_discounts.where("#{quantity} >= quantity")
+      .select('bulk_discounts.*')
+      .group('bulk_discounts.id, merchants.id, items.id')
+      .order(discount_percentage: :desc)
+      .first
+    end
+
+    def discounted_revenue
+      if best_discount
+        (1 - best_discount.discount_percentage.to_f / 100) * (quantity * unit_price)
+      else
+        total_revenue
+      end
+    end
+
+    def total_revenue
+      (quantity * unit_price)
+    end
 end
